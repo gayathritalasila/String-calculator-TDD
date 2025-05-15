4b65c3d6f89f809c7a648367cdbfc7e64e367436
gayathritalasila <gayathritalasila36@gmail.com>
2025-05-15 14:57:51 +0530

    test: return number for single input

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -1,14 +1,19 @@
-import { StringCalculator} from "../stringCalculator";
+import { StringCalculator } from "../stringCalculator";
 
-describe("String Calculator", ()=>{
-    let calculator: StringCalculator;
+describe("String Calculator", () => {
+  let calculator: StringCalculator;
 
   beforeEach(() => {
     calculator = new StringCalculator();
   });
 
-    it("should return 0 for empty string", ()=>{
-        expect(calculator.add("")).toBe(0);
-    });
+  it("should return 0 for empty string", () => {
+    expect(calculator.add("")).toBe(0);
+  });
+
+  it("returns the number itself for a single number", () => {
+    expect(calculator.add("1")).toBe(1);
+    expect(calculator.add("5")).toBe(5);
+  });
 
 });
