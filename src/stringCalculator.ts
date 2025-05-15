a09d9c2714b272e23cf35c27b249fcd26d0b9be0
gayathritalasila <gayathritalasila36@gmail.com>
2025-05-15 14:58:41 +0530

    feat: return single number if no delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,7 @@
 export class StringCalculator {
     add(input: string): number {
         if (input === "") return 0;
+        if (!input.includes(",")) return parseInt(input, 10);
         return 0;
     }
 }
